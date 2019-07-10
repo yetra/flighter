0ca1440d69147d436c85654c176457952cec21b9
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-10 12:21:12 +0200

    Add method for fetching weather for single city

--- app/components/open_weather_map.rb
+++ app/components/open_weather_map.rb
@@ -1,2 +1,14 @@
 module OpenWeatherMap
+  def self.city(city_name)
+    city_id = OpenWeatherMap::Resolver.city_id(city_name)
+
+    return if city_id.nil?
+
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    response = Faraday.get(
+      url, id: city_id, appid: Rails.application.credentials.open_weather_map_api_key
+    )
+
+    OpenWeatherMap::City.parse(JSON.parse(response.body))
+  end
 end
