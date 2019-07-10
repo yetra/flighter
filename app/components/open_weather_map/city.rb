0f9bb6c9f47804758851a31bad976da11d5aec9b
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-10 11:08:50 +0200

    Initialize City objects from keyword arguments

--- app/components/open_weather_map/city.rb
+++ app/components/open_weather_map/city.rb
@@ -1,4 +1,11 @@
 module OpenWeatherMap
   class City
+    def initialize(id:, lat:, lon:, name:, temp_k:)
+      @id = id
+      @lat = lat
+      @lon = lon
+      @name = name
+      @temp_k = temp_k
+    end
   end
 end
