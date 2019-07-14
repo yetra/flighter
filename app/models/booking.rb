b316867abd182634f2cbeba22bcca2c839c61f64
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-14 23:37:11 +0200

    Add validations for Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,4 +14,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flight
+
+  validates :seat_price, presence: true, numericality: { greater_than: 0 }
+  validates :no_of_seats, presence: true, numericality: { greater_than: 0 }
+
+  validate :flight_not_in_the_past
+
+  def flight_not_in_the_past
+    return unless flight.flys_at.past?
+
+    errors.add(:flight_id, 'flight can\'t be in the past')
+  end
 end
