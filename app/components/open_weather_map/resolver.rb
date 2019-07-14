f5e02d8fbdbdcc2f305b641c35c27a40c72b36e8
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-14 19:43:51 +0200

    Remove redundand json import

--- app/components/open_weather_map/resolver.rb
+++ app/components/open_weather_map/resolver.rb
@@ -1,8 +1,6 @@
 module OpenWeatherMap
   class Resolver
     def self.city_id(city_name)
-      require 'json'
-
       city_ids_path = File.expand_path('city_ids.json', __dir__)
       city_ids_file = File.read(city_ids_path)
       city_ids = JSON.parse(city_ids_file)
