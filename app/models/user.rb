7a079795bafb42256e967ae5143f9c4b949ee640
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-14 23:21:02 +0200

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@
 
 class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  validates :first_name, presence: true, length: { minimum: 2 }
 end
