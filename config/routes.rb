5a3e1d5b6eff519f7844bbf814442c60587a482f
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-15 23:28:25 +0200

    Add routes for all resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:index, :show, :create, :update, :destroy]
+
+    resources :companies, only: [:index, :show, :create, :update, :destroy]
+
+    resources :flights, only: [:index, :show, :create, :update, :destroy]
+
+    resources :bookings, only: [:index, :show, :create, :update, :destroy]
+  end
 end
