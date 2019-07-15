dfa6bfd807f3ca2df6013b591e66676ce545d7fc
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-15 23:09:12 +0200

    Add serializer for Booking

---
+++ app/serializers/booking_serializer.rb
@@ -0,0 +1,12 @@
+class FlightSerializer < ActiveModel::Serializer
+  belongs_to :user
+  belongs_to :flight
+
+  attribute :id
+
+  attribute :seat_price
+  attribute :no_of_seats
+
+  attribute :created_at
+  attribute :updated_at
+end
