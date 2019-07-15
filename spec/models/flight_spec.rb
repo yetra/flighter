58e6aececec8c925e3aeb3c5a35825ec11dd6209
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-15 02:51:11 +0200

    Add specs for Flight model

---
+++ spec/models/flight_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe Flight do
+  describe 'associations' do
+    subject { FactoryBot.create(:flight_with_bookings) }
+
+    it { is_expected.to belong_to(:company) }
+    it { is_expected.to have_many(:bookings) }
+  end
+
+  describe 'validations' do
+    subject(:flight_subject) { FactoryBot.create(:flight) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive.scoped_to(:company_id) }
+
+    it { is_expected.to validate_presence_of(:flys_at) }
+    it { is_expected.to validate_presence_of(:lands_at) }
+
+    it 'has flys_at before lands_at' do
+      expect(flight_subject.flys_at).to be < flight_subject.lands_at
+    end
+
+    it { is_expected.to validate_presence_of(:base_price) }
+    it { is_expected.to validate_numericality_of(:base_price).is_greater_than(0) }
+  end
+end
