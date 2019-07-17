c108192c876ea04736f2a57c96ded1b8f745ea44
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-18 01:40:49 +0200

    Add Companies controller

---
+++ app/controllers/api/companies_controller.rb
@@ -0,0 +1,58 @@
+module Api
+  class CompaniesController < ApplicationController
+    # GET /api/companies(.:format)
+    def index
+      render json: Company.all, status: :ok
+    end
+
+    # POST /api/companies(.:format)
+    def create
+      company = Company.new(company_params)
+
+      if company.save
+        render json: company, status: :created
+      else
+        render json: { errors: company.errors }, status: :bad_request
+      end
+    end
+
+    # GET /api/companies/:id(.:format)
+    def show
+      company = Company.find(params[:id])
+
+      if company
+        render json: company, status: :ok
+      else
+        render json: { errors: company.errors }, status: :bad_request
+      end
+    end
+
+    # PUT /api/companies/:id(.:format)
+    def update
+      company = Company.update(params[:id], company_params)
+
+      if company.valid?
+        render json: company, status: :ok
+      else
+        render json: { errors: company.errors }, status: :bad_request
+      end
+    end
+
+    # DELETE /api/companies/:id(.:format)
+    def destroy
+      company = Company.find(params[:id])
+
+      if company.destroy
+        head :no_content
+      else
+        render json: { errors: company.errors }, status: :bad_request
+      end
+    end
+
+    private
+
+    def company_params
+      params.require(:company).permit(:name, :created_at, :updated_at)
+    end
+  end
+end
