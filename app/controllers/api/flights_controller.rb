6e2431acff0c358b2eb084765adc166955ca9ca3
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-18 01:41:13 +0200

    Add Flights controller

---
+++ app/controllers/api/flights_controller.rb
@@ -0,0 +1,60 @@
+module Api
+  class FlightsController < ApplicationController
+    # GET /api/flights(.:format)
+    def index
+      render json: Flight.all, status: :ok
+    end
+
+    # POST /api/flights(.:format)
+    def create
+      flight = Flight.new(flight_params)
+
+      if flight.save
+        render json: flight, status: :created
+      else
+        render json: { errors: flight.errors }, status: :bad_request
+      end
+    end
+
+    # GET /api/flights/:id(.:format)
+    def show
+      flight = Flight.find(params[:id])
+
+      if flight
+        render json: flight, status: :ok
+      else
+        render json: { errors: flight.errors }, status: :bad_request
+      end
+    end
+
+    # PUT /api/flights/:id(.:format)
+    def update
+      flight = Flight.update(params[:id], flight_params)
+
+      if flight.valid?
+        render json: flight, status: :ok
+      else
+        render json: { errors: flight.errors }, status: :bad_request
+      end
+    end
+
+    # DELETE /api/flights/:id(.:format)
+    def destroy
+      flight = Flight.find(params[:id])
+
+      if flight.destroy
+        head :no_content
+      else
+        render json: { errors: flight.errors }, status: :bad_request
+      end
+    end
+
+    private
+
+    def flight_params
+      params.require(:flight).permit(
+        :company_id, :name, :no_of_seats, :base_price, :flys_at, :lands_at, :created_at, :updated_at
+      )
+    end
+  end
+end
