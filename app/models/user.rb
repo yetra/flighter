cf66bf080cf8d3afa0a567dc70e29a2123441ad2
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-21 17:06:10 +0200

    Add has_secure_password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
-#  first_name :string
-#  last_name  :string
-#  email      :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  first_name      :string
+#  last_name       :string
+#  email           :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
 #
 
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :bookings, dependent: :destroy
 
   validates :email, presence: true, uniqueness: { case_sensitive: false },
