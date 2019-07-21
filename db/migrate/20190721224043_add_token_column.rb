8600d6a6a6364626829968211c1efecba164ff68
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-22 00:49:40 +0200

    Add token column migration

---
+++ db/migrate/20190721224043_add_token_column.rb
@@ -0,0 +1,6 @@
+class AddTokenColumn < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :token, :string
+    add_index :users, :token, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_080414) do
+ActiveRecord::Schema.define(version: 2019_07_21_224043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,9 @@ ActiveRecord::Schema.define(version: 2019_07_21_080414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
   add_foreign_key "bookings", "flights"
