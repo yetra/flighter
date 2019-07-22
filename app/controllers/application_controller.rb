6dad583331709226e427ed1ce0c9feda6ece7593
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-22 19:35:26 +0200

    Add support for token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  def require_login
+    authenticate_token || render_unauthenticated
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  protected
+
+  def render_unauthorized
+    errors = { errors: { credentials: ['are invalid'] } }
+
+    render json: errors, status: :bad_request
+  end
+
+  def render_unauthenticated
+    errors = { errors: { token: ['is invalid'] } }
+
+    render json: errors, status: :unauthorized
+  end
+
+  private
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _|
+      User.find_by(token: token)
+    end
+  end
 end
