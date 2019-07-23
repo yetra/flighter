ddf6a2e150bd30dc7513bb17b26ef95a76b5878e
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-23 19:32:33 +0200

    Move Companies API index and show specs to new files

---
+++ spec/requests/api/companies/show_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'Companies API show', type: :request do
+  include TestHelpers::JsonResponse
+
+  let!(:companies) { FactoryBot.create_list(:company, 3) }
+
+  describe 'GET /api/companies/:id(.:format)' do
+    it 'returns a single company' do
+      get "/api/companies/#{companies.first.id}",
+          headers: api_headers
+
+      expect(response).to have_http_status(:ok)
+      expect(json_body['company']).to include('id', 'flights', 'created_at', 'updated_at')
+      expect(json_body['company']).to include('name' => companies.first.name)
+    end
+  end
+end
