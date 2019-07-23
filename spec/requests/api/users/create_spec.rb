f849cfbfc69baa4e356bf55808b0b156424c662b
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-23 16:54:47 +0200

    Use user_params in Users API create specs

--- spec/requests/api/users/create_spec.rb
+++ spec/requests/api/users/create_spec.rb
@@ -1,21 +1,20 @@
 RSpec.describe 'Users API create', type: :request do
   include TestHelpers::JsonResponse
-
-  let(:users) { FactoryBot.create_list(:user, 3) }
-  let(:valid_user) { FactoryBot.build(:user, first_name: 'Name', email: 'a@b.com') }
+  include TestHelpers::JsonParams
 
   describe 'POST /api/users(.:format)' do
     context 'when params are valid' do
       it 'creates a user' do
         expect do
           post '/api/users',
-               params: { user: valid_user.attributes }.to_json,
+               params: user_params.to_json,
                headers: api_headers
         end.to change(User, :count).by(1)
 
         expect(response).to have_http_status(:created)
-        expect(json_body['user']).to include('first_name' => valid_user.first_name,
-                                             'email' => valid_user.email)
+        expect(json_body['user']).to include('first_name' => user_params[:user][:first_name],
+                                             'last_name' => user_params[:user][:last_name],
+                                             'email' => user_params[:user][:email])
       end
     end
 
@@ -23,7 +22,7 @@ RSpec.describe 'Users API create', type: :request do
       it 'returns 400 Bad Request' do
         expect do
           post '/api/users',
-               params: { user: { name: '', email: '' } }.to_json,
+               params: user_params(first_name: '', email: '').to_json,
                headers: api_headers
         end.not_to change(User, :count)
 
