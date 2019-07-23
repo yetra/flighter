b63013c0cfae92c9c006f7dea90bdfc15106ece1
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-23 20:56:26 +0200

    Fix invalid_params typo in users create specs

--- spec/requests/api/users/create_spec.rb
+++ spec/requests/api/users/create_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'Users API create', type: :request do
   include TestHelpers::JsonResponse
 
-  let(:valid_parms) { FactoryBot.attributes_for(:user) }
+  let(:valid_params) { FactoryBot.attributes_for(:user) }
   let(:invalid_params) { FactoryBot.attributes_for(:user, first_name: '', email: '', password: '') }
 
   describe 'POST /api/users(.:format)' do
@@ -9,23 +9,23 @@ RSpec.describe 'Users API create', type: :request do
       it 'creates a user' do
         expect do
           post '/api/users',
-               params: { user: valid_parms }.to_json,
+               params: { user: valid_params }.to_json,
                headers: api_headers
         end.to change(User, :count).by(1)
 
         expect(response).to have_http_status(:created)
-        expect(json_body['user']).to include('first_name' => valid_parms[:first_name],
-                                             'last_name' => valid_parms[:last_name],
-                                             'email' => valid_parms[:email])
+        expect(json_body['user']).to include('first_name' => valid_params[:first_name],
+                                             'last_name' => valid_params[:last_name],
+                                             'email' => valid_params[:email])
       end
 
       it 'registers created user with provided password' do
         post '/api/users',
-             params: { user: valid_parms }.to_json,
+             params: { user: valid_params }.to_json,
              headers: api_headers
 
         user = User.find(json_body['user']['id'])
-        expect(user.authenticate(valid_parms[:password])).to eq(user)
+        expect(user.authenticate(valid_params[:password])).to eq(user)
       end
     end
 
