2c50107bc299377d538698266daade365c6cfffe
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-23 17:07:18 +0200

    Add tests for user registration

--- spec/requests/api/users/create_spec.rb
+++ spec/requests/api/users/create_spec.rb
@@ -16,18 +16,27 @@ RSpec.describe 'Users API create', type: :request do
                                              'last_name' => user_params[:user][:last_name],
                                              'email' => user_params[:user][:email])
       end
+
+      it 'registers created user with provided password' do
+        post '/api/users',
+             params: user_params(email: 'a2@b.com').to_json,
+             headers: api_headers
+
+        user = User.find(json_body['user']['id'])
+        expect(user.authenticate(user_params[:user][:password])).to eq(user)
+      end
     end
 
     context 'when params are invalid' do
       it 'returns 400 Bad Request' do
         expect do
           post '/api/users',
-               params: user_params(first_name: '', email: '').to_json,
+               params: user_params(first_name: '', email: '', password: '').to_json,
                headers: api_headers
         end.not_to change(User, :count)
 
         expect(response).to have_http_status(:bad_request)
-        expect(json_body['errors']).to include('first_name', 'email')
+        expect(json_body['errors']).to include('first_name', 'email', 'password')
       end
     end
   end
