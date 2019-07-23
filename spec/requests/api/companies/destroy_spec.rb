dc6ccb5bd13d64f2d7cff36ee4aaf6e94c9cd132
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-23 19:33:19 +0200

    Update Companies API destroy specs

---
+++ spec/requests/api/companies/destroy_spec.rb
@@ -0,0 +1,43 @@
+RSpec.describe 'Companies API destroy', type: :request do
+  include TestHelpers::JsonResponse
+
+  let!(:public_user) { FactoryBot.create(:user) }
+  let!(:admin_user) { FactoryBot.create(:user, admin: true) }
+
+  let!(:company) { FactoryBot.create(:company) }
+
+  describe 'DELETE /api/companies/id(.:format)' do
+    context 'when unauthenticated' do
+      it 'returns 401 Unauthorized' do
+        expect do
+          delete "/api/companies/#{company.id}",
+                 headers: api_headers
+        end.not_to change(Company, :count)
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when unauthorized' do
+      it 'forbids non-admins to delete companies' do
+        expect do
+          delete "/api/companies/#{company.id}",
+                 headers: api_headers(token: public_user.token)
+        end.not_to change(Company, :count)
+
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'when authorized' do
+      it 'allows admins to delete companies' do
+        expect do
+          delete "/api/companies/#{company.id}",
+                 headers: api_headers(token: admin_user.token)
+        end.to change(Company, :count).by(-1)
+
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+  end
+end
