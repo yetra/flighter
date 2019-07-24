40f55d976cc07ce65c21c98d4fb582f757860064
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-24 23:51:27 +0200

    Remove user_params role except from Users API create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,7 +14,7 @@ module Api
 
     # POST /api/users(.:format)
     def create
-      user = User.new(user_params.except('role'))
+      user = User.new(user_params)
 
       if user.save
         render json: user, status: :created
