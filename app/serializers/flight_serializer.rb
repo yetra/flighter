d8a697041146f70d928f40c4fc9fe74ad4d931c3
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-28 20:00:55 +0200

    Add number of booked seats for each flight

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -6,6 +6,8 @@ class FlightSerializer < ActiveModel::Serializer
   attribute :name
 
   attribute :no_of_seats
+  attribute :no_of_booked_seats
+
   attribute :base_price
 
   attribute :flys_at
@@ -13,4 +15,8 @@ class FlightSerializer < ActiveModel::Serializer
 
   attribute :created_at
   attribute :updated_at
+
+  def no_of_booked_seats
+    object.booked_seats
+  end
 end
