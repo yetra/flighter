f05ba169be265881421e60cfeb68762491a19dbc
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-29 11:50:18 +0200

    Allow only admins to see statistics data

--- app/controllers/api/statistics/companies_controller.rb
+++ app/controllers/api/statistics/companies_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module Statistics
     class CompaniesController < ApplicationController
+      before_action :require_login
+      before_action :require_permission
+
       def index
         render json: Company.all,
                each_serializer: Statistics::CompanySerializer,
--- app/controllers/api/statistics/flights_controller.rb
+++ app/controllers/api/statistics/flights_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module Statistics
     class FlightsController < ApplicationController
+      before_action :require_login
+      before_action :require_permission
+
       def index
         render json: Flight.all,
                each_serializer: Statistics::FlightSerializer,
