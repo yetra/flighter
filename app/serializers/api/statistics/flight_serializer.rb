1f7d4c1a11bb06047949d85a659b807b5ee3cd7a
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-29 15:10:05 +0200

    Move statistics serializers to api folder

---
+++ app/serializers/api/statistics/flight_serializer.rb
@@ -0,0 +1,23 @@
+module Api
+  module Statistics
+    class FlightSerializer < ActiveModel::Serializer
+      attributes :flight_id, :revenue, :no_of_booked_seats, :occupancy
+
+      def flight_id
+        object.id
+      end
+
+      def revenue
+        object.booked_seats * object.base_price
+      end
+
+      def no_of_booked_seats
+        object.booked_seats
+      end
+
+      def occupancy
+        no_of_booked_seats / object.no_of_seats.to_f
+      end
+    end
+  end
+end
