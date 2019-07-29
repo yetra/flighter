bde3120e56c68c42ddb9b1e62264b13492869def
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-30 00:14:26 +0200

    Add test for query param in Users API index

--- spec/requests/api/users/index_spec.rb
+++ spec/requests/api/users/index_spec.rb
@@ -1,7 +1,10 @@
 RSpec.describe 'Users API index', type: :request do
   include TestHelpers::JsonResponse
 
-  before { FactoryBot.create_list(:user, 3) }
+  before do
+    FactoryBot.create_list(:user, 3)
+    FactoryBot.create(:user, last_name: 'TestName')
+  end
 
   let(:public_user) { FactoryBot.create(:user) }
   let(:admin_user) { FactoryBot.create(:user, admin: true) }
@@ -33,7 +36,16 @@ RSpec.describe 'Users API index', type: :request do
             headers: api_headers(token: admin_user.token)
 
         expect(response).to have_http_status(:ok)
-        expect(json_body['users'].count).to eq(4)
+        expect(json_body['users'].count).to eq(5)
+      end
+
+      it 'supports filtering by email, first_name or last_name' do
+        get '/api/users?query=test',
+            headers: api_headers(token: admin_user.token)
+
+        expect(response).to have_http_status(:ok)
+        expect(json_body['users'].count).to eq(1)
+        expect(json_body['users'].first).to include('last_name' => 'TestName')
       end
     end
   end
