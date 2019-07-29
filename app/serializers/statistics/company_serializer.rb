2671a01a989934dc9a919815c800634ab5f25d7a
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-29 11:47:28 +0200

    Add Statistics::CompanySerializer attributes

--- app/serializers/statistics/company_serializer.rb
+++ app/serializers/statistics/company_serializer.rb
@@ -1,4 +1,21 @@
 module Statistics
   class CompanySerializer < ActiveModel::Serializer
+    attributes :company_id, :total_revenue, :total_no_of_booked_seats, :average_price_of_seats
+
+    def company_id
+      object.id
+    end
+
+    def total_revenue
+      object.flights.sum { |flight| flight.booked_seats * flight.current_price }
+    end
+
+    def total_no_of_booked_seats
+      object.flights.sum(&:booked_seats)
+    end
+
+    def average_price_of_seats
+      total_revenue / total_no_of_booked_seats
+    end
   end
 end
