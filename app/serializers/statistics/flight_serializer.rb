0ed74eeff8ce1ed0eafe98869b57f957c6757fb9
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-29 11:45:04 +0200

    Add Statistics::FlightSerializer attributes

--- app/serializers/statistics/flight_serializer.rb
+++ app/serializers/statistics/flight_serializer.rb
@@ -1,4 +1,21 @@
 module Statistics
   class FlightSerializer < ActiveModel::Serializer
+    attributes :flight_id, :revenue, :no_of_booked_seats, :occupancy
+
+    def flight_id
+      object.id
+    end
+
+    def revenue
+      object.booked_seats * object.current_price
+    end
+
+    def no_of_booked_seats
+      object.booked_seats
+    end
+
+    def occupancy
+      no_of_booked_seats / object.no_of_seats
+    end
   end
 end
