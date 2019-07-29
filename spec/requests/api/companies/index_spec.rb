6e625902e8a63c276de9fc605394ca979a02bbad
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-30 01:10:50 +0200

    Fix Companies API index filtering test

--- spec/requests/api/companies/index_spec.rb
+++ spec/requests/api/companies/index_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe 'Companies API index', type: :request do
   include TestHelpers::JsonResponse
 
-  before { FactoryBot.create_list(:company, 3) }
+  let!(:companies) { FactoryBot.create_list(:company, 2) }
+
+  before do
+    FactoryBot.create(:flight, company_id: companies.first.id)
+    FactoryBot.create(:flight, flys_at: DateTime.parse('2015-12-07T19:25:00.000Z'),
+                               company_id: companies.second.id)
+  end
 
   describe 'GET /api/companies(.:format)' do
     it 'successfully returns a list of companies' do
@@ -9,7 +15,15 @@ RSpec.describe 'Companies API index', type: :request do
           headers: api_headers
 
       expect(response).to have_http_status(:ok)
-      expect(json_body['companies'].count).to eq(3)
+      expect(json_body['companies'].count).to eq(2)
+    end
+
+    it 'lists only companies with active flights if fliter query param given' do
+      get '/api/companies?filter=active',
+          headers: api_headers
+
+      expect(response).to have_http_status(:ok)
+      expect(json_body['companies'].count).to eq(1)
     end
   end
 end
