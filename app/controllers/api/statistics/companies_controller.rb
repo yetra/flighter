b1918fb97e1917d401d7570c594c3c8de3e2341e
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-29 11:48:37 +0200

    Implement statistics API index methods

--- app/controllers/api/statistics/companies_controller.rb
+++ app/controllers/api/statistics/companies_controller.rb
@@ -1,6 +1,11 @@
 module Api
   module Statistics
     class CompaniesController < ApplicationController
+      def index
+        render json: Company.all,
+               each_serializer: Statistics::CompanySerializer,
+               status: :ok
+      end
     end
   end
 end
--- app/controllers/api/statistics/flights_controller.rb
+++ app/controllers/api/statistics/flights_controller.rb
@@ -1,6 +1,11 @@
 module Api
   module Statistics
     class FlightsController < ApplicationController
+      def index
+        render json: Flight.all,
+               each_serializer: Statistics::FlightSerializer,
+               status: :ok
+      end
     end
   end
 end
