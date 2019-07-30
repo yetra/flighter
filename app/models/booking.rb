bdb515aa8bd92eed4ae6962979908bc3d4baa9f8
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-30 16:35:37 +0200

    Fix booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -21,6 +21,8 @@ class Booking < ApplicationRecord
   validate :flight_not_in_the_past
   validate :flight_not_overbooked
 
+  before_validation :set_seat_price, on: :create
+
   scope :active, -> { joins(:flight).merge(Flight.active) }
 
   def flight_not_in_the_past
@@ -30,8 +32,18 @@ class Booking < ApplicationRecord
   end
 
   def flight_not_overbooked
-    return if flight && no_of_seats && (flight.booked_seats + no_of_seats <= flight.no_of_seats)
+    return if flight && no_of_seats && (booked_seats + no_of_seats <= flight.no_of_seats)
 
     errors.add(:flight, "can't be overbooked")
+  end
+
+  def booked_seats
+    flight.bookings.where.not(id: id).sum(&:no_of_seats)
+  end
+
+  private
+
+  def set_seat_price
+    self.seat_price = flight.current_price
   end
 end
