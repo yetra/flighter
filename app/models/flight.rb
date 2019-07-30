09f34f4410181375e5a793c23e3ec0354e14a9b3
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-30 16:35:20 +0200

    Fix flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -31,39 +31,31 @@ class Flight < ApplicationRecord
   scope :active, -> { where('flys_at > ?', DateTime.now) }
 
   scope :name_cont, ->(string) { where('name ILIKE ?', "%#{string}%") }
-  scope :flys_at_eq, ->(timestamp) { where(flys_at: DateTime.parse(timestamp)) }
+  scope :flys_at_eq, ->(timestamp) { where('DATE(flys_at) = ?', Date.parse(timestamp)) }
   scope :no_of_available_seats_gteq, lambda { |seats|
     where(id: Flight.left_joins(:bookings).group(:id)
                     .having('flights.no_of_seats - COALESCE(SUM(bookings.no_of_seats), 0) >= ?',
                             seats.to_i))
   }
 
-  scope :overlapping_flys_at, lambda { |flight|
-    where('(lands_at >= :time) AND (flys_at < :time)', time: flight.flys_at)
-      .where('company_id = ?', flight.company_id)
-  }
-  scope :overlapping_lands_at, lambda { |flight|
-    where('(flys_at <= :time) AND (lands_at > :time)', time: flight.lands_at)
-      .where('company_id = ?', flight.company_id)
-  }
+  def overlapping
+    company.flights.where('(flights.flys_at, flights.lands_at) OVERLAPS (?, ?)', flys_at, lands_at)
+           .where.not(id: id)
+  end
 
   def booked_seats
     bookings.sum(&:no_of_seats)
   end
 
   def current_price
-    return base_price unless flys_at - DateTime.now <= 15.days
+    diff_in_days = (flys_at.to_datetime - DateTime.now).to_i
 
-    last_minute_price
+    return base_price if diff_in_days >= 15
+
+    ((2 - [diff_in_days, 0].max / 15.0) * base_price).round
   end
 
   private
-
-  def last_minute_price
-    diff_in_days = [(flys_at.to_datetime - DateTime.now).to_i, 0].max
-
-    ((2 - diff_in_days / 15.0) * base_price).round
-  end
 
   def flys_before_lands
     return unless flys_at && lands_at && flys_at >= lands_at
@@ -71,16 +63,10 @@ class Flight < ApplicationRecord
     errors.add(:flys_at, 'must be before lands_at')
   end
 
-  def overlapping_flys_at?
-    self.class.overlapping_flys_at(self).any?
-  end
-
-  def overlapping_lands_at?
-    self.class.overlapping_lands_at(self).any?
-  end
-
   def overlapping_flights
-    errors.add(:flys_at, "can't overlap within the same company") if overlapping_flys_at?
-    errors.add(:lands_at, "can't overlap within the same company") if overlapping_lands_at?
+    return if company.blank? || company.flights.blank? || overlapping.empty?
+
+    errors.add(:flys_at, "can't overlap within the same company")
+    errors.add(:lands_at, "can't overlap within the same company")
   end
 end
