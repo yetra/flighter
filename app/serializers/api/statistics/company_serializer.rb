c910b8e0dd6eb64a6e50b5f6093d32f41f43aa70
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-30 03:02:02 +0200

    Add default values for statistics API serializers

--- app/serializers/api/statistics/company_serializer.rb
+++ app/serializers/api/statistics/company_serializer.rb
@@ -1,13 +1,21 @@
 module Api
   module Statistics
     class CompanySerializer < ActiveModel::Serializer
+      include ActionView::Helpers::NumberHelper
+
       attribute :company_id
 
-      attribute :total_revenue
+      attribute :total_revenue do
+        object[:total_revenue] || 0
+      end
 
-      attribute :total_no_of_booked_seats
+      attribute :total_no_of_booked_seats do
+        object[:total_no_of_booked_seats] || 0
+      end
 
-      attribute :average_price_of_seats
+      attribute :average_price_of_seats do
+        number_to_percentage(object[:total_no_of_booked_seats] * 100, precision: 2) || '0.0%'
+      end
     end
   end
 end
--- app/serializers/api/statistics/flight_serializer.rb
+++ app/serializers/api/statistics/flight_serializer.rb
@@ -1,13 +1,21 @@
 module Api
   module Statistics
     class FlightSerializer < ActiveModel::Serializer
+      include ActionView::Helpers::NumberHelper
+
       attribute :flight_id
 
-      attribute :revenue
+      attribute :revenue do
+        object[:revenue] || 0
+      end
 
-      attribute :no_of_booked_seats
+      attribute :no_of_booked_seats do
+        object[:no_of_booked_seats] || 0
+      end
 
-      attribute :occupancy
+      attribute :occupancy do
+        number_to_percentage(object[:occupancy] * 100, precision: 2) || '0.0%'
+      end
     end
   end
 end
