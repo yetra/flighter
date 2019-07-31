2476776e2e406747bb26b865b267d8daf271a667
Bruna Dujmovic <brdujmovic@gmail.com>
2019-07-31 22:00:29 +0200

    Fix average_price_of_seats calculation in CompaniesQuery

--- app/queries/companies_query.rb
+++ app/queries/companies_query.rb
@@ -9,7 +9,8 @@ class CompaniesQuery
     relation.select('companies.id as company_id')
             .select('SUM(bookings.no_of_seats * bookings.seat_price) AS total_revenue')
             .select('SUM(bookings.no_of_seats) AS total_no_of_booked_seats')
-            .select('AVG(bookings.seat_price) AS average_price_of_seats')
+            .select('SUM(bookings.no_of_seats * bookings.seat_price) / SUM(bookings.no_of_seats)
+                     AS average_price_of_seats')
             .left_joins(flights: :bookings)
             .group(:id)
   end
